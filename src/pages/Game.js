2468ef928f700fd0ebcecc5e89dc17f0bae57a86
Yujean Shin <yujeanshin@college.harvard.edu>
2023-11-29 20:57:42 -0500

    added a grid in the game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,28 +1,54 @@
-import React, {useState} from 'react'
-import Grid from '../components/Grid'
+import React, { useState } from 'react'
 import Timer from '../components/Timer'
+import Block from '../components/Block'
 import '../styles/Game.css'
 
 import { Link, useNavigate } from 'react-router-dom';
 
-function Game({grid}) {
+const numRows = 10;
+const numCols = 17;
+
+// create numRows x numCols array of numbers
+let grid = []
+for (let row = 0; row < numRows; row ++) {
+    grid.push(Array(5).fill(0));
+    for (let col = 0; col < numCols; col ++) {
+        // random integer from 1 to 9
+        grid[row][col] = Math.floor(Math.random() * 9) + 1;
+    }
+}
+console.log(grid);
+
+function Game() {
+    let newGrid = [];
+    newGrid = grid.map((numbers, index) => 
+        <tr key={index}>
+            {numbers.map((number) => 
+                <td><Block number={number}/></td>
+            )}
+        </tr>
+    );
+
     const navigate = useNavigate();
     const [totalSeconds, setTotalSeconds] = useState(120);
 
     const handleSecondsChange = (newSeconds) => {
         setTotalSeconds(newSeconds);
     };
-    if (totalSeconds === 117) {
+
+    if (totalSeconds === 0) {
         navigate('/gameover');
     }
-
+    
     return (
     <>
     <div id="header">
         <span>Timer: <Timer onSecondsChange={handleSecondsChange} /> </span>
         <span>Apple Game</span>
     </div>
-    <Grid grid={grid}/>
+    <table>
+        {newGrid}
+    </table>
     <Link to="/">Return to Home</Link>
     </>
     );
