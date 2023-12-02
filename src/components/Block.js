df124c4da7dbc2c59dad02785f73356e799d3212
Yujean Shin <yujeanshin@college.harvard.edu>
2023-12-02 00:02:07 -0500

    blocks are removed but then select doesn't function well

--- src/components/Block.js
+++ src/components/Block.js
@@ -5,7 +5,7 @@ function Block({id, active, className, number, onClick}) {
 
   return (
     <>
-    <div id={id} number={number} className={`${className} ${active ? 'active' : ''}`} >
+    <div id={id} number={number} className={`${className} ${(active) ? 'active' : ''}`} >
         <div className="block-container">
             <div className="block">{number}</div>
         </div>
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,6 +7,8 @@ import { ReactMouseSelect } from 'react-mouse-select'
 const numRows = 5;
 const numCols = 5;
 
+const target = 10;
+
 // create numRows x numCols array of numbers
 let gridVals = []
 for (let row = 0; row < numRows; row++) {
@@ -14,19 +16,22 @@ for (let row = 0; row < numRows; row++) {
     for (let col = 0; col < numCols; col++) {
         // random integer from 1 to 9
         gridVals[row][col] = {
-            id: [row, col],
+            id: row + "," + col,
             className: "block-container-container no-select-text",
-            number: Math.floor(Math.random() * 9) + 1
+            number: Math.floor(Math.random() * 9) + 1,
         };
     }
 }
 
 
 function Grid() {
-    // selection box
+    const [grid, setGrid] = useState(gridVals);
+
     const containerRef = useRef(null);
 
+    // "is the selection box in use"?
     const [isSelecting, setIsSelecting] = useState(false);
+    // items that are currently selected with the selection box
     const [selectedItems, setSelectedItems] = useState([]);
 
     const handleStartSelection = () => {
@@ -35,14 +40,42 @@ function Grid() {
     }
     const finishSelection = (items, e) => {
         setIsSelecting(false);
+        
+        // get selected items
         const selectedIds = items.map(item => item.getAttribute('id'));
+        console.log(selectedIds)
         setSelectedItems(selectedIds);
+        console.log(selectedItems)
+        
+        // get sum of selected numbers
         const numbers = items.map(item => parseInt(item.getAttribute('number')));
         let sum = numbers.reduce((runningSum, current) => {return runningSum + current}, 0);
+        
+        // if the sum is the target, add .removed class
+        if (sum === target) {
+            // add .removed to appropriate blocks
+            let nextState = grid.map((blockRow, rowIndex) => 
+                {blockRow.map((block, colIndex) => {
+                    console.log(block.id)
+                    console.log(selectedIds)
+                    if (selectedIds.includes(block.id)) {
+                        console.log(block.id);
+                        let blockTemp = block;
+                        blockTemp.className += " removed";
+                        return blockTemp;
+                    }
+                    else { return block; }
+                });
+            return blockRow;
+        });
+            console.log(nextState)
+            setGrid(nextState);
+            console.log(grid)
+        }
     }
 
-
-    let displayGrid = gridVals.map((blockRow, rowIndex) =>
+    // active: 2 means target, 1 means simply selected, 0 means no change
+    let displayGrid = grid.map((blockRow, rowIndex) =>
         <tr key={rowIndex}>
             {blockRow.map((block, colIndex) => {
                 return (
